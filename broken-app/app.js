2b37edadd5f9d737cca81c2245019f8dc3d440e5
Nicholas L <NLoo1@outlook.com>
2024-03-27 00:07:57 -0400

    Fixed app.js. Refactored to synchronous function with .then and .catch. Created map of developers, then resolved promises rather than doing both at the same time

--- broken-app/app.js
+++ broken-app/app.js
@@ -4,21 +4,39 @@ var app = express();
 
 app.use(express.json())
 
-app.post('/', async function(req, res, next) {
-  console.log(req.body)
+app.post('/', function(req, res, next) {
   try {
-    // Using a JSON body of developers, 
-    let results = req.body.developers.map(async developer => {
-      await axios.get(`https://api.github.com/users/${developer}`);
+    // Using a JSON body of developers 
+    let developers = req.body.developers; 
+    let promises = developers.map(developer => {
+      return axios.get(`https://api.github.com/users/${developer}`)
+        .then(response => {
+          console.log(`Acquired data for ${developer}`);
+          return { name: response.data.login, bio: response.data.bio }; 
+        })
+        .catch(err => {
+          next(err)
+        });
     });
 
-    console.log(results)
-    // let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
-
-    // return res.send(JSON.stringify(out));
-  } catch(err) {
-    next(err);
+    Promise.all(promises)
+      .then(results => {
+        return res.send(JSON.stringify(results)); // Send response after all promises are resolved
+      })
+      .catch(err => {
+        next(err);
+      });
+  } catch (err) {
+    next(err); 
   }
+});
+
+app.use((err, req, res, next) => {
+  let status = err.response ? err.response.status : 500;
+  let message = err.message
+  return res.status(status).json({
+    error: { message, status }
+  });
 });
 
 app.listen(3000);
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,6 +1,8 @@
 # Broken App Issues
-- Added app.use(express.json()) to read JSON
-- Installed express.
+- Added app.use(express.json()) to read JSON. JSON would not be parsed otherwise
+- Instead of mapping developers and sending GET request in the same block, mapped developers to promises, then called Promise.all(). This removes the issue of HTTP headers being sent multiple times and throwing an error
+- With the addition of .then, added .catch blocks to promises for further error handling.
+- Installed express. App will not function otherwise
 - Changed mapping variable name from d to username for clarity
 - Added err to catch block to ensure functional try/catch
-- Removed return from axios.get. 
+- Changed async functions to instead use .then blocks. Async function would not properly wait for response.
